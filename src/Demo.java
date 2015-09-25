69ca6db188ddc2c68abcced4f6dac3695f3a1c92
Sugarcoder <mikichea@gmail.com>
2015-09-25 14:39:15 -0700

    LinkedList demo

--- src/Demo.java
+++ src/Demo.java
@@ -1,4 +1,6 @@
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
     // psvm = shortcut to "public static void main"
@@ -16,13 +18,50 @@ public class Demo {
 
 
         printList(placesToVisit);
+
+        placesToVisit.add(1, "Alice Springs");
+        printList(placesToVisit);
+
+
+        placesToVisit.remove(4);
+        printList(placesToVisit);
+
+    }
+
+
+    public static void printList(LinkedList<String> linkedList) {
+        Iterator<String> i=linkedList.iterator();
+
+        while(i.hasNext()) {
+            System.out.println("Now visiting " + i.next());
+        }
+        System.out.println("==========");
     }
 
 
 
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
 
+        while (stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
 
-
+            if (comparison == 0) {
+                // equal, do not add
+                System.out.println(newCity + " is already included as a destination");
+                return false;
+            } else if (comparison > 0) {
+                // new city should appear before this one
+                // Brisbane -> Adelaide
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if (comparison < 0) {
+                // move to the next city
+            }
+        }
+        stringListIterator.add(newCity);
+    }
 
 
 }
