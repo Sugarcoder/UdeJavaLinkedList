83054c75593b07ed23a616dd16e3922a90e3d1aa
Sugarcoder <mikichea@gmail.com>
2015-09-25 13:46:19 -0700

    LinkedList demo

---
+++ src/Demo.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+public class Demo {
+    // psvm = shortcut to "public static void main"
+
+    public static void main(String[] args) {
+
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Canberra");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Darwin");
+
+
+        printList(placesToVisit);
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Sugarcoder on 9/24/2015.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 4 bites in each integer
+
+
+        Customer customer = new Customer("Tim", 20.50);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+
+        System.out.println("Balance for " + customer.getName() + " is " + customer.getBalance());
+
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for(int i=0; i<intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+
+        for(int i=0; i<intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+    }
+}
